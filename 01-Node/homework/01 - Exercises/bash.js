610f6684ce57ce3dfde0549a69a1d91e10d0ffd8
lisandroviedo <lisandroviedo@hotmail.com>
2023-10-31 19:31:26 -0400

    M3 31/10/2023

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,11 +1,11 @@
-const process = require('process');
-const { Z_ASCII } = require('zlib');
-const commands = require('./commands/index.js');
+const process = require("process");
+const { Z_ASCII } = require("zlib");
+const commands = require("./commands/index.js");
 
 function bash() {}
 
 bash();
 module.exports = {
-   print,
-   bash,
+  print,
+  bash,
 };
