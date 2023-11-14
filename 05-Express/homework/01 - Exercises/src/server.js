4b05846e2f33dc33503844334d5330e44f688d25
Lisandro <lisandroviedo@hotmail.com>
2023-11-14 11:46:02 -0400

    M3 14/11/2023

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -6,5 +6,17 @@ const server = express();
 
 server.use(express.json());
 
+server.post("/posts", function (req, res) {
+  if (req.body.author && req.body.title && req.body.contents) {
+    publications.push(req.body);
+    res.json(publications);
+  } else {
+    res.status(404).json({
+      error:
+        "No se recibieron los parámetros necesarios para crear la publicación",
+    });
+  }
+});
+
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = { publications, server };
