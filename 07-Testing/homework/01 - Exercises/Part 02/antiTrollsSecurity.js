1a69e31cc2fb1330a185ad41987090cf60de9d9b
lisandroviedo <lisandroviedo@hotmail.com>
2023-11-18 20:56:07 -0400

    M3 18/11/2023

--- 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
+++ 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
@@ -1,3 +1,9 @@
-const antiTrollsSecurity = (string) => {};
+const antiTrollsSecurity = (string) => {
+  if (typeof string !== "string") {
+    throw new Error("El parámetro recibido debe ser un string");
+  }
+
+  return string.replace(/[aeiouAEIOU]/g, "").trim();
+};
 
 module.exports = antiTrollsSecurity;
--- 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.test.js
+++ 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.test.js
@@ -1,14 +1,60 @@
-const antiTrollsSecurity = require('./antiTrollsSecurity');
+const antiTrollsSecurity = require("./antiTrollsSecurity");
 
-describe('PARTE 02', () => {
-   describe('Seguridad Anti Trolls', () => {
-      it('Debe devolver el mismo string pero habiendo eliminado todas las vocales', () => {
-         expect(antiTrollsSecurity('This website is for losers LOL!')).toBe(
-            'Ths wbst s fr lsrs LL!'
-         );
-         expect(antiTrollsSecurity('What are you, a communist?')).toBe(
-            'Wht r y,  cmmnst?'
-         );
-      });
-   });
+describe("PARTE 02", () => {
+  describe("Seguridad Anti Trolls", () => {
+    it("Debes crear una función", () => {
+      expect(typeof antiTrollsSecurity).not.toBe("undefined");
+      expect(typeof antiTrollsSecurity).not.toBe("object");
+      expect(typeof antiTrollsSecurity).not.toBe("boolean");
+      expect(typeof antiTrollsSecurity).not.toBe("number");
+      expect(typeof antiTrollsSecurity).not.toBe("bigint");
+      expect(typeof antiTrollsSecurity).not.toBe("string");
+      expect(typeof antiTrollsSecurity).not.toBe("symbol");
+      expect(typeof antiTrollsSecurity).toBe("function");
+    });
+
+    it("La función debe llamarse antiTrollsSecurity", () => {
+      expect(antiTrollsSecurity.name).toBe("antiTrollsSecurity");
+    });
+
+    it("Debe arrojar un error en caso de recibir un parámetro que no sea un string", () => {
+      expect(() => antiTrollsSecurity(null)).toThrow(
+        "El parámetro recibido debe ser un string"
+      );
+      expect(() => antiTrollsSecurity({})).toThrow(
+        "El parámetro recibido debe ser un string"
+      );
+      expect(() => antiTrollsSecurity([])).toThrow(
+        "El parámetro recibido debe ser un string"
+      );
+      expect(() => antiTrollsSecurity(true)).toThrow(
+        "El parámetro recibido debe ser un string"
+      );
+      expect(() => antiTrollsSecurity(false)).toThrow(
+        "El parámetro recibido debe ser un string"
+      );
+      expect(() => antiTrollsSecurity(24)).toThrow(
+        "El parámetro recibido debe ser un string"
+      );
+    });
+
+    it("Debe devolver el mismo string pero habiendo eliminado todas las vocales", () => {
+      expect(antiTrollsSecurity("This website is for losers LOL!")).toBe(
+        "Ths wbst s fr lsrs LL!"
+      );
+      expect(antiTrollsSecurity("What are you, a communist?")).toBe(
+        "Wht r y,  cmmnst?"
+      );
+    });
+
+    it("No debe contener espacios en blanco al principio o al final del string", () => {
+      expect(antiTrollsSecurity(" This website is for losers LOL!")).toBe(
+        "Ths wbst s fr lsrs LL!"
+      );
+      expect(antiTrollsSecurity("What are you, a communist? ")).toBe(
+        "Wht r y,  cmmnst?"
+      );
+      expect(antiTrollsSecurity(" Hello World ")).toBe("Hll Wrld");
+    });
+  });
 });
